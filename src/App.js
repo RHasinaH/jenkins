01f3025a9e73b61aa5156bcfed304dbd16ae9354
bob <randriatahina31@gmail.com>
2022-09-14 11:43:12 +0300

    ci/cd ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
         <p>
-          CI/CD <code>src/App.js</code> and save to reload.
+         test CI/CD <code>src/App.js</code> and save to reload.
         </p>
     </div>
   );
